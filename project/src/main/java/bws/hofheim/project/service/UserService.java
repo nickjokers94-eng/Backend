e1d10367304fc65d0db17f2ba758191b590590a3
nickj <nick.jokers94@gmail.com>
2025-09-13 16:27:27 +0200

    zwischesntand

--- project/src/main/java/bws/hofheim/project/service/UserService.java
+++ project/src/main/java/bws/hofheim/project/service/UserService.java
@@ -17,12 +17,10 @@ public class UserService {
 
         try (Connection conn = DriverManager.getConnection(url, user, dbPassword);
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT userid, username, role, status FROM users WHERE username =" + username)) {
+             ResultSet rs = stmt.executeQuery("SELECT userid, username, role, status FROM users WHERE username = '" + username + "'")) {
 
-            while (rs.next()) {
-                System.out.println("ppppppppppppp");
+            if (rs.next()) {
                 User userInDB = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
-                System.out.println(user);
                 return userInDB;
             }
         } catch (SQLException ex) {
@@ -35,10 +33,26 @@ public class UserService {
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String user = "postgres";
         String dbPassword = "worti";
-
+        // Prüfen, ob Username schon existiert
         try (Connection conn = DriverManager.getConnection(url, user, dbPassword);
-             Statement stmt = conn.createStatement()) {
-            int changedRows = stmt.executeUpdate("INSERT INTO users (username, password, role, status) VALUES ('" + username + "', '" + password + "', 'user', 'locked')");
+             PreparedStatement checkStmt = conn.prepareStatement("SELECT COUNT(*) FROM users WHERE username = ?")) {
+            checkStmt.setString(1, username);
+            ResultSet rs = checkStmt.executeQuery();
+            if (rs.next() && rs.getInt(1) > 0) {
+                // Username existiert bereits!
+                return false;
+            }
+        } catch (SQLException ex) {
+            System.err.println("Fehler: " + ex.getMessage());
+            return false;
+        }
+        // Hinzufügen, wenn Username noch nicht existiert
+        try (Connection conn = DriverManager.getConnection(url, user, dbPassword);
+             PreparedStatement stmt = conn.prepareStatement(
+                     "INSERT INTO users (username, password, role, status) VALUES (?, ?, 'user', 'locked')")) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            int changedRows = stmt.executeUpdate();
             return changedRows > 0;
         } catch (SQLException ex) {
             System.err.println("Fehler: " + ex.getMessage());
